3abcb7b1a4984e514f91e9b20b6d14596c8537a1
pablomagub <pablomagub@gmail.com>
2016-11-14 00:11:51 +0100

    tienda terminada

--- tiendaonline/master.js
+++ tiendaonline/master.js
@@ -1,5 +1,7 @@
-var librosId=["ijs","ijq","jsa","jqa","dboo"];
+var librosId=["ijs","ijq","jsa","jqa","dbo"];
 var libros=["Introdución a JavaScript","Introducción a Jquery","Javascript Avanzado","Jquery Avanzado","Desarrollo con bootstrap"];
+var vacia=document.getElementById('vacia');
+var cesta=document.getElementById('listacesta');
 function setCookie(nombre,valor,caducuidad,dominio,ruta,segura){
   if (arguments.length < 2) {
     return null;
@@ -21,15 +23,14 @@ function setCookie(nombre,valor,caducuidad,dominio,ruta,segura){
   }
   document.cookie=cookie;
 }
-
 function getCookie(nameCookie){
   if (document.cookie.length>0) {
     cookies=document.cookie.split(";");
 
     for (var i = 0; i < cookies.length; i++) {
       cookie=cookies[i].split("=");
-      name=decodeURIComponent(cookie[0]);
-      value=decodeURIComponent(cookie[1]);
+      name=decodeURIComponent(cookie[0]).trim();
+      value=decodeURIComponent(cookie[1]).trim();
       if(name==nameCookie){
         return value;
       }
@@ -39,33 +40,55 @@ function getCookie(nameCookie){
 }
 
 function verCesta(){
-  cesta=document.getElementById('listacesta');
-  document.write("<li onclick='rmcesta("+item+")'>"+libros[librosId.indexOf(item)]+"</li>");
+  cesta.innerHTML="";
+  if (document.cookie.length>0) {
+    vacia.style.display="none";
+    cookies=document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      cookie=cookies[i].split("=");
+      name=decodeURIComponent(cookie[0]).trim();
+      value=decodeURIComponent(cookie[1]);
+      indice=librosId.indexOf(name);
+      libro=libros[indice];
+      cesta.innerHTML+="<li><button type='button' name='button' onclick=\"rmCesta('"+name+"')\"> - </button>"+libro+" ("+value+")"+"</li>";
+    }
+    return null;
+  }else {
+    vacia.style.display="inline";
+  }
+
+
 }
 function vaciarCesta(){
   if (document.cookie.length>0) {
     cookies=document.cookie.split(";");
-
     for (var i = 0; i < cookies.length; i++) {
       cookie=cookies[i].split("=");
       name=decodeURIComponent(cookie[0]);
       value=decodeURIComponent(cookie[1]);
-      //imprimir linea de libro
+      setCookie(name,valor,0);
     }
+    verCesta();
     return null;
   }
 }
 function addCesta(item) {
   cookieItem=getCookie(item);
   if(cookieItem!=null){
-      setCookie(parseInt(cookieItem)+1)
+      setCookie(item,parseInt(cookieItem)+1);
+  }else{
+    setCookie(item,1);
   }
+  cookieItem=getCookie(item);
 }
 function rmCesta(item) {
   cookieItem=getCookie(item);
   if(cookieItem!=null){
-      if(parseInt(cookieItem)<0){
-        setCookie(parseInt(cookieItem)-1);
+      if(parseInt(cookieItem)>1){/*get cookie devuelve el valor de la cookie que en este caso es el numero de libros de ese item*/
+        setCookie(item,parseInt(cookieItem)-1);
+      }else {
+        setCookie(item,0,0);
       }
+      verCesta();
   }
 }
